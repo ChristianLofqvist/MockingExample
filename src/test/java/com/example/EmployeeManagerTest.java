1a78dbc21d31c18ad07516a8f4285a03f333839a
ChristianLofqvist <christian.lofqvist@iths.se>
2022-01-24 12:58:09 +0100

    Add test for payEmployees should catch exception

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -3,7 +3,10 @@ package com.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class EmployeeManagerTest {
 
@@ -15,5 +18,22 @@ class EmployeeManagerTest {
         var result = employeeManager.payEmployees();
 
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("payEmployees should catch RuntimeException")
+    void payEmployeesShouldCatchRuntimeException() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        BankService bankService = mock(BankService.class);
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+        doThrow(new RuntimeException()).when(bankService).pay("Test", 2.0f);
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("Test", 2.0f),
+                new Employee("Test2", 3.0f), new Employee("Test3", 4.0f)));
+
+        var result = employeeManager.payEmployees();
+        var result1 = employeeRepository.findAll();
+
+        assertThat(result).isEqualTo(2);
+        assertThat(result1).hasSize(3);
     }
 }
