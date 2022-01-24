da1d42e68822938e7a1f9b1f98cb1e9db4b41d5f
ChristianLofqvist <christian.lofqvist@iths.se>
2022-01-24 12:28:51 +0100

    Add test for payEmployees
    Test should return correct number of employees.

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,19 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class EmployeeManagerTest {
 
+    @Test
+    @DisplayName("Checking payEmployees should return correct number of employees")
+    void payEmployeesShouldReturnNumberOfEmployees() {
+        EmployeeManager employeeManager = new EmployeeManager(new EmployeeRepositoryStub(), new BankServiceStub());
+
+        var result = employeeManager.payEmployees();
+
+        assertThat(result).isEqualTo(3);
+    }
 }
